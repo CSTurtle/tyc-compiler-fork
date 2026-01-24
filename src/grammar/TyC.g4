d7b1b1c211a3136a2bd881cc5cde0bb8906f5bd2
Turtle <hmhuy605@gmail.com>
2026-01-25 00:52:24 +0700

    Lexer unfinished constructs and testing

--- src/grammar/TyC.g4
+++ src/grammar/TyC.g4
@@ -27,8 +27,28 @@ options{
 // TODO: Define grammar rules here
 program: EOF;
 
-WS : [ \t\r\n]+ -> skip ; // skip spaces, tabs
+WS : [ \t\f\r\n]+ -> skip ; // skip spaces, tabs
+
+BLK_CMT: '/*' .*? '*/' -> skip ;
+
+LINE_CMT: '//' ~('\n' | '\r')* -> skip ;
+
+KW: 'auto' | 'break' | 'case' | 'continue' | 'default' | 'else' | 'float' | 'for' | 'if' | 'int' | 'return' | 'string' | 'struct' | 'switch' | 'void' | 'while' ;
+
+ID: [a-zA-Z_]  [a-zA-Z0-9_]* ;
+
+OP: ('+' | '-' | '*' | '/' | '%' | '==' | '!=' | '<' | '<=' | '>' | '>=' | '&&' | '||' | '!' | '++' | '--' | '.' | '=') ;
+
+SEP: ('[' | ']' | '{' | '}' | '(' | ')' | ';' | ',') ;
+
+INT_LIT: '0' | ('-'? [1-9] [0-9]*);
+
+// FLOAT_LIT: ('0'? '.' [0-9]+) | ('-'?) ;
+
+// ESC: ('\b' | '\f' | '\r' | '\n' | '\t' | '\\"' | '\\\\') ;
+
+// STRING_LIT: '"' (ESC | ~('\\' | '"'))* '"'  ;
 
 ERROR_CHAR: .;
-ILLEGAL_ESCAPE:.;
-UNCLOSE_STRING:.;
+ILLEGAL_ESCAPE: .;
+UNCLOSE_STRING: .;
--- tests/test_lexer.py
+++ tests/test_lexer.py
@@ -6,10 +6,28 @@ TODO: Implement 100 test cases for lexer
 import pytest
 from tests.utils import Tokenizer
 
+def compare_results(source, expected_result):
+    for s, r in zip(source, expected_result):
+        tokenizer = Tokenizer(s)
+        print(tokenizer.get_tokens_as_string())
+        # TODO: Add actual test assertions
+        assert tokenizer.get_tokens_as_string() == r
 
 def test_lexer_placeholder():
     """Placeholder test - replace with actual test cases"""
-    source = "// This is a placeholder test"
-    tokenizer = Tokenizer(source)
-    # TODO: Add actual test assertions
-    assert True
+    source = ["// This is a placeholder test", "/* This is a block comment \n This is a block comment */", "PPL", "auto"]
+    result = ["EOF", "EOF", "ID,PPL,EOF", "KW,auto,EOF"]
+
+    compare_results(source, result)
+
+def test_lexer_keywords():
+    source = ["auto", "break", "case", "continue", "default", "else", "float", "for", "if", "int", "return", "string", "struct", "switch", "void", "while"]
+    result = ["KW,auto,EOF", "KW,break,EOF", "KW,case,EOF", "KW,continue,EOF", "KW,default,EOF", "KW,else,EOF", "KW,float,EOF", "KW,for,EOF", "KW,if,EOF", "KW,int,EOF", "KW,return,EOF", "KW,string,EOF", "KW,struct,EOF", "KW,switch,EOF", "KW,void,EOF", "KW,while,EOF"]
+
+    compare_results(source, result)
+
+def test_escape_characters():
+    source=["\"\\b\"", "\"\\f\"", "\"\\r\"", "\"\\n\"", "\"\\t\"", "\"\\\"\"", "\"\\\\\"", "\"\\b\\f\\r\\n\\t\\\"\\\\\""]
+    result = ["STRING_LIT,\"\\b\",EOF", "STRING_LIT,\"\\f\",EOF", "STRING_LIT,\"\\r\",EOF", "STRING_LIT,\"\\n\",EOF", "STRING_LIT,\"\\t\",EOF", "STRING_LIT,\"\\\"\",EOF", "STRING_LIT,\"\\\\\",EOF", "STRING_LIT,\"\\b\\f\\r\\n\\t\\\"\\\\\",EOF"]
+
+    compare_results(source, result)
